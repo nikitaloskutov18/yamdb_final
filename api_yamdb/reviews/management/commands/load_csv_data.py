e41de1063e74b02adf6ab578d0204de5af5e3a1e
Nikita <nikitaloskutov18@gmail.com>
2023-04-15 21:47:35 +0300

    Fuck flake8 test2

--- api_yamdb/reviews/management/commands/load_csv_data.py
+++ api_yamdb/reviews/management/commands/load_csv_data.py
@@ -36,7 +36,7 @@ def open_csv_file(file_name):
             return list(csv.reader(file))
     except FileNotFoundError:
         print(f'Файл {csv_file} не найден.')
-        return
+        return None
 
 
 def change_foreign_values(data_csv):
